471c8ec6e71c741ebbf836f6ac6885e69e56829b
Thomas Bradford <thom@kode4food.it>
2023-03-29 15:52:12 +0200

    update example

--- join/main.go
+++ join/main.go
@@ -5,34 +5,36 @@ import (
 	"math/rand"
 
 	"github.com/caravan/essentials"
-	"github.com/caravan/essentials/topic"
+	"github.com/caravan/essentials/message"
+	"github.com/caravan/streaming/stream"
 	"github.com/caravan/streaming/stream/build"
 )
 
 func main() {
 	// Create new topics with permanent retention
-	left := essentials.NewTopic()
-	right := essentials.NewTopic()
-	out := essentials.NewTopic()
+	left := essentials.NewTopic[stream.Event]()
+	right := essentials.NewTopic[stream.Event]()
+	out := essentials.NewTopic[stream.Event]()
+	msg := message.Of[stream.Event]()
 
 	s, _ := build.
 		TopicSource(left).
-		Filter(func(e topic.Event) bool {
+		Filter(func(e stream.Event) bool {
 			// Filter out numbers greater than or equal to 200
 			return e.(int) < 200
 		}).
 		Join(
 			build.
 				TopicSource(right).
-				Filter(func(e topic.Event) bool {
+				Filter(func(e stream.Event) bool {
 					// Filter out numbers less than or equal to 100
 					return e.(int) > 100
 				}),
-			func(l topic.Event, r topic.Event) bool {
+			func(l stream.Event, r stream.Event) bool {
 				// Only join if the left is even, and the right is odd
 				return l.(int)%2 == 0 && r.(int)%2 == 1
 			},
-			func(l topic.Event, r topic.Event) topic.Event {
+			func(l stream.Event, r stream.Event) stream.Event {
 				// Join by multiplying the numbers
 				return l.(int) * r.(int)
 			},
@@ -46,17 +48,17 @@ func main() {
 		lp := left.NewProducer()
 		rp := right.NewProducer()
 		for i := 0; i < 10000; i++ {
-			_ = lp.Send(rand.Intn(1000))
-			_ = rp.Send(rand.Intn(1000))
+			msg.Send(lp, rand.Intn(1000))
+			msg.Send(rp, rand.Intn(1000))
 		}
-		_ = lp.Close()
-		_ = rp.Close()
+		lp.Close()
+		rp.Close()
 	}()
 
 	c := out.NewConsumer()
 	for i := 0; i < 10; i++ {
 		// Display the first ten that come out
-		fmt.Println(topic.MustReceive(c))
+		fmt.Println(msg.MustReceive(c))
 	}
-	_ = c.Close()
+	c.Close()
 }
